60bfbe5efdd02b548f589475035743fa76911865
sccodeworks <chroops@gmail.com>
2025-04-28 12:28:43 +0000

    Update from https://github.com/sccodeworks/SCC-Website/commit/03d961ba698ef77cfaa0c18166c1003cd5bf4505

---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-REIDLZYG.mjs";import"./chunk-XPKKGQFE.mjs";import"./chunk-NCKEQJPH.mjs";import"./chunk-XXQQDCEL.mjs";import"./chunk-H6IVHZPH.mjs";import"./chunk-GJY7G2D4.mjs";import"./chunk-C7ACHG65.mjs";import"./chunk-COT65Y5O.mjs";export{a as default};
